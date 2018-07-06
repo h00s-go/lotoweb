070ab6861a4bb707d3e09bde58400c40c1fa54da
Krunoslav Husak <kruno@binel.hr>
2018-07-06 03:02:48 +0200

    Added model function which generates numbers

--- app/controllers/loto_controller.go
+++ app/controllers/loto_controller.go
@@ -4,16 +4,18 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/h00s/lotoweb/app/models"
 	"github.com/labstack/echo"
 )
 
 // LotoController for Loto methods
 type LotoController struct {
+	lotoModel *models.LotoModel
 }
 
 // NewLotoController creates new loto controller
 func NewLotoController() *LotoController {
-	return &LotoController{}
+	return &LotoController{models.NewLotoModel()}
 }
 
 // Index is first page
@@ -21,10 +23,12 @@ func (lc *LotoController) Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "loto/index", template.HTML(""))
 }
 
+// Loto6od45 return numbers for loto 6 od 45
 func (lc *LotoController) Loto6od45(c echo.Context) error {
-	return c.Render(http.StatusOK, "loto/6od45", "This is loto 6/45")
+	return c.Render(http.StatusOK, "loto/6od45", lc.lotoModel.GetNumbers(6, 45))
 }
 
+// Loto7od39 return numbers for loto 7 od 39
 func (lc *LotoController) Loto7od39(c echo.Context) error {
-	return c.Render(http.StatusOK, "loto/7od39", "This is loto 7/39")
+	return c.Render(http.StatusOK, "loto/7od39", lc.lotoModel.GetNumbers(7, 39))
 }
--- app/models/loto_model.go
+++ app/models/loto_model.go
@@ -1,1 +1,57 @@
 package models
+
+import (
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// LotoModel for Loto methods
+type LotoModel struct {
+}
+
+// NewLotoModel creates new loto model
+func NewLotoModel() *LotoModel {
+	return &LotoModel{}
+}
+
+// GetNumbers returns numbers
+func (lm *LotoModel) GetNumbers(count int, max int) string {
+	var numbers []int
+	rand.Seed(time.Now().UnixNano())
+
+	for len(numbers) < count {
+		n := rand.Intn(max) + 1
+		if !contains(numbers, n) {
+			numbers = append(numbers, n)
+		}
+	}
+
+	sort(numbers)
+	var numbersAsText []string
+	for _, i := range numbers {
+		numbersAsText = append(numbersAsText, strconv.Itoa(i))
+	}
+
+	return strings.Join(numbersAsText, ", ")
+}
+
+func contains(s []int, v int) bool {
+	for _, a := range s {
+		if a == v {
+			return true
+		}
+	}
+	return false
+}
+
+func sort(s []int) {
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s)-1; j++ {
+			if s[j] > s[j+1] {
+				s[j], s[j+1] = s[j+1], s[j]
+			}
+		}
+	}
+}
